0e1d325c707bd830a5a9cc253e4c995be9f3ceca
Eugene “Aeron” Glybin <aeron@aeron.cc>
2013-02-25 00:13:03 +0700

    Setup and .gitignore updated.

--- setup.py
+++ setup.py
@@ -3,8 +3,14 @@ from setuptools import setup
 
 setup(
 	name="pyflowdock",
-	version="0.2",
-	py_modules=['flowdock'],
+	version="0.2.19",
+	packages=[
+		'flowdock',
+		'flowdock.helpers',
+	],
+	package_dir={
+		'flowdock': 'flowdock'
+	},
 	scripts=[],
 	install_requires=['requests'],
 	package_data={
@@ -16,10 +22,15 @@ setup(
 	license="LGPLv3",
 	keywords="flowdock api wrapper",
 	classifiers=[
-		'Development Status :: 4 - Beta',
-		'Environment :: Console',
-		'Intended Audience :: Developers',
-		'License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)',
-		'Topic :: Communications'
+		"Development Status :: 4 - Beta",
+		"Intended Audience :: Developers",
+		"License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)",
+		"Topic :: Communications",
+		"Operating System :: OS Independent",
+		"Programming Language :: Python :: 2",
+		"Programming Language :: Python :: 2.6",
+		"Programming Language :: Python :: 2.7",
+		"Programming Language :: Python :: Implementation :: CPython",
+		"Programming Language :: Python :: Implementation :: PyPy",
 	]
 )
